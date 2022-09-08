39dc3e6123eed5016ad09ff99fcc9aeca3805fa6
Kevin Z <kevin.zaffino@gammadia.ch>
2022-09-08 10:27:39 +0200

    Upgrade dbal and Use a custom uuid generator to pass some tests

--- tests/Fixtures/Doctrine/BlogPost.php
+++ tests/Fixtures/Doctrine/BlogPost.php
@@ -19,7 +19,8 @@ use JMS\Serializer\Annotation\XmlRoot;
 class BlogPost
 {
     /**
-     * @ORM\Id @ORM\Column(type="guid") @ORM\GeneratedValue(strategy="UUID")
+     * @ORM\Id @ORM\Column(type="guid") @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
      */
     protected $id;
 
